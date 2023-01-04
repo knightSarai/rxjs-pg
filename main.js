4322164626b62263959d4d016106fb1412cd4f1e
knightSarai <knight.sarai.dev@gmail.com>
2023-01-04 21:21:01 +0300

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Rxjs Playground</h1>
+    <div style="display: flex; flex-direction: column; align-items: center;">
+        <button id="btn" onclick>Click me</button>
+    </div>
+  </div>
+`
