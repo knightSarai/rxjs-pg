472bf857bb44c7ebb458a4d40df47b48ee9df674
knightSarai <knight.sarai.dev@gmail.com>
2023-01-06 21:40:11 +0300

    Create loading streams

---
+++ race/script.js
@@ -0,0 +1,36 @@
+import { fromEvent, concat, of, race, timer } from 'rxjs';
+import { tap, exhaustMap, delay, shareReplay, first } from 'rxjs/operators';
+
+import {
+  responseTimeField,
+  showLoadingAfterField,
+  showLoadingForAtLeastField,
+  loadingStatus,
+  showLoading,
+  form,
+  fetchData,
+} from './utilities';
+
+
+const loading$ = fromEvent(form, 'submit').pipe(
+  exhaustMap(() => {
+    const data$ = fetchData().pipe(shareReplay(1));
+
+    const showLoading$ = of(true).pipe(
+      delay(+showLoadingAfterField.value),
+      tap(showLoading)
+    )
+
+    const timeToHideTheLoading$ = timer(+showLoadingForAtLeastField).pipe(first())
+
+    const shouldShowLoading$ = concat(
+      showLoading$,
+      timeToHideTheLoading$,
+      data$.pipe(tap(() => showLoading(false))),
+    )
+
+    return race(data$, shouldShowLoading$)
+  })
+)
+
+loading$.subscribe();
---
+++ race/utilities.js
@@ -0,0 +1,38 @@
+import { tap, of, delay } from 'rxjs';
+
+export const form = document.querySelector('form');
+
+export const loadingStatus = document.getElementById('loading-status');
+export const responseTimeField = document.getElementById('response-time');
+export const showLoadingAfterField =
+  document.getElementById('show-loading-after');
+export const showLoadingForAtLeastField = document.getElementById(
+  'show-loading-for-at-least',
+);
+
+export const fetchData = () => {
+  const responseTime = +responseTimeField.value;
+  const showAfter = +showLoadingAfterField.value;
+  const showFor = +showLoadingForAtLeastField.value;
+
+  return of(true).pipe(
+    tap(() => console.log('Fetching…', responseTime)),
+    delay(responseTime),
+    tap(() => {
+      console.log('Data: Recieved');
+    }),
+  );
+};
+
+export const showLoading = (loading) => {
+  console.log({ loading });
+  if (loading) {
+    loadingStatus.innerHTML = '<div class="currently-loading">Loading…</div>';
+  } else {
+    loadingStatus.innerHTML = '';
+  }
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
